e24992021b2cd9c47605fd8ba26bd47cf0022c1f
Fabian Mendez <ofmendez@gmail.com>
2021-01-07 00:53:20 -0500

    first commit

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <header class="header-global">
+        <base-nav class="navbar-main" transparent type="" effect="light" expand>
+            <router-link slot="brand" class="navbar-brand mr-lg-5" to="/">
+                <img src="img/brand/white.png" alt="logo">
+            </router-link>
+
+
+            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="https://github.com/ofmendez/ftorre_wa"
+                       target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
+                        <i class="fa fa-github"></i>
+                        <span class="nav-link-inner--text d-lg-none">Github</span>
+                    </a>
+                </li>
+                <li class="nav-item d-none d-lg-block ml-lg-4">
+                    <a href="https://torre.co/en/search/people" target="_blank" rel="noopener"
+                       class="btn btn-neutral btn-icon">
+                        <span class="nav-link-inner--text">Go To Torre</span>
+                    </a>
+                </li>
+            </ul>
+        </base-nav>
+    </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+};
+</script>
+<style>
+</style>
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = {
+  configureWebpack: {
+    // Set up all the aliases we use in our app.
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 6
+      })
+    ]
+  },
+  pwa: {
+    name: 'FTorre_WA',
+    themeColor: '#32cd32',
+    msTileColor: '#32cd32',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#32cd32'
+  },
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== 'production'
+  }
+};
